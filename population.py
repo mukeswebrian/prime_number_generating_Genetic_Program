4a4e4798e08564722a548701cad526da92b81a24
Brian <mukeswebrian@yahoo.com>
2019-11-08 12:52:38 -0800

    added new methods

--- population.py
+++ population.py
@@ -2,11 +2,14 @@ import individual
 
 class Population():
     
-    def __init__(self, population_size, a_range, b_range, min_depth, max_depth, pset):
+    def __init__(self, init_population_size, a_range, b_range, min_depth, max_depth, pset):
+        
+        # fitness of the fittest individual in the population (intialized as 0)
+        self.max_fitness = 0
         
         # create a population of tree individuals
         self.population = {}
-        for i in range(0, population_size):
+        for i in range(0, init_population_size):
             self.population[i] = individual.Individual(a_range, b_range, pset, min_depth, max_depth)
             self.population[i].make_tree()
      
@@ -14,8 +17,20 @@ class Population():
         return self.population   
     
     def get_individual(self, index):
+        return self.population[index]
         
-        if index < len(self.polulation) and index >=0:
-            return self.population[index]
-        else:
-            return None
+        
+    def add_individual(self, ind1):
+        
+        next_id = max(self.population.keys()) + 1
+        self.population[next_id] = ind1
+        
+    def remove_individual(self, ind_id):
+        self.population.pop(ind_id)
+        
+    def get_size(self):
+        return len(self.population)
+    
+    def describe(self):
+        for key in self.population.keys():
+            print(str(key) +' : '+ self.population[key].describe())
